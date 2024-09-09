7f6c70165dd46d26fbcfb1888ab70649e72c0b63
Takafuji Keita <135183240+takahujikeita@users.noreply.github.com>
2024-09-09 22:12:10 +0800

    test

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
+    //createメソッドの記述
     function create()
     {
         return view('todo.create');
     }
 
+    //editメソッドの記述
     function edit()
     {
         return view('todo.edit');
