a76f20daf0f945a48cbf28467a7583b32d767234
Takafuji Keita <135183240+takahujikeita@users.noreply.github.com>
2024-09-11 04:28:11 +0800

    crete.blade.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Task;
+use Illuminate\Support\Facades\Auth; 
+
 class TaskController extends Controller
 {
 
@@ -15,6 +18,35 @@ class TaskController extends Controller
     function create()
     {
         return view('todo.create');
+    }
+
+    //storeメソッドの記述
+    function store(Request $request)
+    {
+
+        // バリデーションを追加
+    $request->validate([
+        'title' => 'required|string|max:255',
+        'contents' => 'required|string',
+        'image_at' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048', // 画像のサイズや拡張子のバリデーション
+        
+    ]);
+    
+        // dd($request);
+        $task = new Task;
+        $task->title = $request->title;
+        $task->contents = $request->contents;
+        $task->user_id = Auth::id();
+
+        // 画像ファイルがアップロードされているか確認
+        if ($request->hasFile('image_at')) {
+            // 画像を保存し、ファイルパスを取得
+            $imagePath = $request->file('image_at')->store('images', 'public');
+            $task->image_at = $imagePath; // ファイルパスをデータベースに保存
+        }
+
+        $task->save();
+        return redirect()->route('task.index');
     }
 
     //editメソッドの記述
--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/create.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    {{-- <link rel="stylesheet" href="{{ asset('css/edit.css') }}"> --}}
 
 </head>
 <body>
 
-    <div class="container mt-5">
+    {{-- <div class="container mt-5">
         <div><h1>投稿してみよう！</h1></div>
   <div class="row justify-content-center">
     <div class="col-md-8">
@@ -28,7 +29,49 @@
       </form>
   </div>
 </div>
-</div>
+</div> --}}
+
+<main>
+  <div class="container">
+    <div>
+    <form action=" {{route('task.store')}} " method="POST" class="row justify-content-center" enctype="multipart/form-data">
+        @csrf
+
+        <h2 class="text-center mt-3 mb-5">投稿してみよう！</h2>
+        <div class="col-md-10">
+          <label for="#" class="form-label fs-3" >title</label>
+          <input type="text" class="form-control form-control-lg border-dark-subtle" id="formGroupExampleInput" value="" name="title">
+          <small><p class="text-end">30文字以内</p></small>
+        </div>
+        <div class="col-md-10">
+          <label for="#" class="form-label fs-3">contents</label>
+          <textarea class="form-control form-control-lg border-dark-subtle" id="exampleFormControlTextarea1" rows="5" value="" name="contents"></textarea>
+          <small><p class="text-end">140文字以内</p></small>
+        </div>
+        <dic class="col-md-10">
+          <div class="row mb-5">
+            <label for="#" class="form-label fs-3" >image</label>
+            <div class="col-md-7">
+              <input type="file" class="form-control form-control-lg border-dark-subtle" name="image_at" required>
+            </div>
+            
+          </div>
+        </dic>
+        <div class="d-md-flex justify-content-md-center mb-5">
+          <div class="col-md-3">
+            <button type="submit" class="form-control btn btn-primary btn-lg">新規投稿作成</button>
+          </div>
+        </div>
+      <div class="d-md-flex justify-content-between mb-5">
+          <div class="col-md-15">
+            <input type="button" class="form-control btn btn-outline-secondary" value="＜ cancel" action="{{route('task.index')}}">
+            {{-- <a href="{{route('task.index')}}"class="form-control btn btn-outline-secondary" ></a>  --}}
+          </div>
+    </form>
+    </div>
+    </div>
+  </div>
+</main>
 
 </body>
 </html>
